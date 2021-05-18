e7bf37ca07fbdafdc4036dd62e61d82d469af83a
RebeccaPan <xinyipan@sjtu.edu.cn>
2021-05-18 10:51:24 +0800

    Project2: 2.2 2.3 add buddy and page_table

--- kernel/memory/answer_buddy.h
+++ kernel/memory/answer_buddy.h
@@ -1,11 +1,13 @@
 // 提示：把元数据页加入到空闲池子里
 void _buddy_return_page(page_t* page) {
-    // TODO: make merge here
     // Suggested: 4 LoCs
+    page->flags = BD_PAGE_FREE;
+    bd_lists[page->orders].nr_free++;
+    list_add(&page->list_node, &bd_lists[page->orders].list_head);
 }
 
 void _buddy_get_specific_page(page_t* page) {
-    page->flags |= BD_PAGE_IN_USE;
+    page->flags = BD_PAGE_IN_USE;
     bd_lists[page->orders].nr_free--;
     list_del(&page->list_node);
 }
@@ -15,44 +17,69 @@ void _buddy_clear_flag(page_t* page) { page->flags = 0; }
 // 提示：buddy伙伴页的元数据位于整个内存可分配区域的最前面
 // 该函数给出一个buddy伙伴页的元数据指针
 // 返回元数据所指向的页是全局从数据区开始的第几个页
-uint64 _buddy_get_page_idx(page_t* page) { return /* Your return value */NULL; }
+uint64 _buddy_get_page_idx(page_t* page) { return page - bd_meta.first_meta_page; }
 
 // 提示：请仔细阅读kfree函数的代码
-uint64 _buddy_get_area_idx(void* head) { return /* Your return value */NULL; }
+uint64 _buddy_get_area_idx(void* head) { return ((uint64)head - bd_meta.data_head) / PGSIZE; }
 
 // 提示：给出一个全局页的下标，返回这个页的元数据指针
 page_t* _buddy_idx_get_page(uint64 idx) {
-    return /* Your return value */NULL;
+    return bd_meta.first_meta_page + idx;
 }
 
 // 提示：给出一个页的元数据指针，找到这个页的buddy页的元数据指针
 // 注意利用buddy页的一些性质
 page_t* _buddy_get_buddy_page(page_t* page) {
     // Suggested: 2 LoCs
-    return /* Your return value */NULL;
+    return _buddy_idx_get_page(_buddy_get_page_idx(page) ^ (1 << page->orders));
 }
 
 // 提示：给出一个buddy页的元数据，将这个页分裂成至少出现1个target_order的函数
 void _buddy_split_page(page_t *original_page, uint64 target_order){
     // Suggested: 5 LoCs
+    bd_lists[original_page->orders].nr_free--;
+    list_del(&original_page->list_node);
+    while (original_page->orders > target_order) {
+        original_page->orders--;
+        page_t *buddy_page = _buddy_get_buddy_page(original_page);
+        buddy_page->orders = original_page->orders;
+        buddy_page->flags = BD_PAGE_FREE;
+        bd_lists[buddy_page->orders].nr_free++;
+        list_add(&buddy_page->list_node, &bd_lists[original_page->orders].list_head);
+    }
+    original_page->flags = BD_PAGE_IN_USE;
 }
 
 // 提示：分配一个order为order的页
 page_t *_buddy_alloc_page(uint64 order){
     // Suggested: 13 LoCs
-    return /* Your return value */NULL;
+    uint32 cur_order = order;
+    while (cur_order < bd_max_size && bd_lists[cur_order].nr_free == 0) { cur_order++; }
+    if (cur_order == bd_max_size && bd_lists[cur_order].nr_free == 0) {
+        return NULL; // allocate failure due to lack of space
+    }
+    page_t *cur_page = list_entry(bd_lists[cur_order].list_head.next, page_t, list_node);
+    _buddy_split_page(cur_page, order);
+    return cur_page;
 }
 
 void buddy_free_page(page_t* page) {
     page_t* current_page = page;
-    for (; current_page->orders < bd_max_size; ++current_page->orders) {
+    for (; current_page->orders < bd_max_size - 1; ++current_page->orders) {
         page_t* buddy_page = _buddy_get_buddy_page(current_page);
-        if ((!(buddy_page->flags & BD_PAGE_FREE))) {
+        if ((!((buddy_page->flags & BD_PAGE_FREE) && current_page->orders == buddy_page->orders))) {
             break;
         }
-        _buddy_get_specific_page(buddy_page);
+        buddy_page->flags = BD_PAGE_IN_USE;
+        bd_lists[buddy_page->orders].nr_free--;
+        list_del(&buddy_page->list_node);
+//        _buddy_get_specific_page(buddy_page);
         _buddy_clear_flag(buddy_page);
-        if(_buddy_get_page_idx(current_page) > _buddy_get_page_idx(buddy_page))current_page = buddy_page;
+        if(_buddy_get_page_idx(current_page) > _buddy_get_page_idx(buddy_page)){
+            buddy_page->flags = BD_PAGE_IN_USE;
+            _buddy_clear_flag(current_page);
+            current_page = buddy_page;
+        }
     }
     _buddy_return_page(current_page);
 }
--- kernel/memory/answer_pgt.h
+++ kernel/memory/answer_pgt.h
@@ -1,8 +1,9 @@
 void enable_paging() {
-    // TODO: Homework 2: Enable paging
     // After initializing the page table, write to register SATP register for kernel registers.
     // Flush the TLB to invalidate the existing TLB Entries
     // Suggested: 2 LoCs
+    w_satp(MAKE_SATP(kernel_pagetable));
+    flush_tlb();
 }
 
 // Return the address of the PTE in page table *pagetable*
@@ -14,29 +15,57 @@ void enable_paging() {
 //   21...29 -- 9  bits of level-1 index
 //   12...20 -- 9  bits of level-0 index
 //   0...11  -- 12 bits of byte offset within the page
-// Return the last-level page table entry.
+// Return the last-level page table entry (WITHOUT checking it's validity).
 static pte_t* pt_query(pagetable_t pagetable, vaddr_t va, int alloc){
     if(va >= MAXVA) BUG_FMT("get va[0x%lx] >= MAXVA[0x%lx]", va, MAXVA);
     // Suggested: 18 LoCs
-    return /* Return value here */ NULL;
+    pte_t *pte;
+    for (int level = 3; level >= 1; --level) {
+        pte = &pagetable[PX(level, va)];
+        if (*pte & PTE_V) { // is valid
+            pagetable = (pagetable_t) PTE2PA(*pte);
+        } else { // not valid
+            if (!alloc) return NULL;
+            pagetable = (pde_t*) mm_kalloc();
+            if (pagetable == NULL) return NULL;
+            memset(pagetable, 0, BD_LEAF_SIZE);
+            *pte = PA2PTE(pagetable) | PTE_V; // set valid
+        }
+    }
+    return &pagetable[PX(0, va)];
 }
 int pt_map_pages(pagetable_t pagetable, vaddr_t va, paddr_t pa, uint64 size, int perm){
     // Suggested: 11 LoCs
+    uint64 va_end = PGROUNDDOWN(va + size - 1);
+    va = PGROUNDDOWN(va);
+    while (true) {
+        pt_map_addrs(pagetable, va, pa, perm);
+        if (va == va_end) break;
+        va += PGSIZE; pa += PGSIZE;
+    }
     return 0; // Do not modify
 }
 
 paddr_t pt_query_address(pagetable_t pagetable, vaddr_t va){
     // Suggested: 3 LoCs
-    pt_query(0, 0, 1);
-    return /* Return value here */ 0;
+    pte_t *pte = pt_query(pagetable, va, 0);
+    // not found or found invalid address: return addr = 0
+    if (pte == NULL || !(*pte & PTE_V)) return 0;
+    return (paddr_t)(PTE2PA(*pte) | (va & 0xFFF));
 }
 
 int pt_unmap_addrs(pagetable_t pagetable, vaddr_t va){
     // Suggested: 2 LoCs
+    pte_t *last_level_pte = pt_query(pagetable, va, 0);
+    if (last_level_pte == NULL) return 0;
+    *last_level_pte = 0;
     return 0; // Do not modify
 }
 
 int pt_map_addrs(pagetable_t pagetable, vaddr_t va, paddr_t pa, int perm){
     // Suggested: 2 LoCs
+    pte_t *last_level_pte = pt_query(pagetable, va, 1);
+    if (last_level_pte == NULL) return 0;
+    *last_level_pte = PA2PTE(pa) | perm | PTE_V;
     return 0; // Do not modify
 }
